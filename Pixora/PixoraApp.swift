c9e01ae5afb33b1ae6419089b8ee5507e005dac8
Aryan Rawat <aryanrawat262002@gmail.com>
2025-11-25 10:14:29 +0530

    Initial Commit

---
+++ Pixora/PixoraApp.swift
@@ -0,0 +1,18 @@
+//
+//  PixoraApp.swift
+//  Pixora
+//
+//  Created by Acmeinteh iOS Device  on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PixoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+               // .preferredColorScheme(.light)
+        }
+    }
+}
